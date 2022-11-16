ec405a15f381589934b1166c0e4965552d41657a
Ucheong <113154269+Ucheong@users.noreply.github.com>
2022-11-15 21:41:16 -0500

    DOM

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const DOMSelectors =
+const DOMSelectors = {
+  button: document.querySelector("btn"),
+  form: document.getElementById("form"),
+  input: document.querySelectorAll("input"),
+  box: document.querySelectorAll("text-box"),
+};
+
+DOMSelectors.form.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  console.log(input);
+  /*  DOMSelectors.input.value = ""; */
+});
