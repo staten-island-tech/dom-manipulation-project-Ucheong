7ed7caaae67da14611704cbe3c15f1a5a29be17a
Ucheong <113154269+Ucheong@users.noreply.github.com>
2022-11-17 19:36:14 -0500

    DOM

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let DOMSelectors = {
   button: document.getElementById("btn"),
   form: document.getElementById("form"),
-  input: document.querySelector("input"),
+  input1: document.querySelector(".text-box1"),
+  input2: document.querySelector(".text-box2"),
+  input3: document.querySelector(".text-box3"),
   box: document.getElementById("form-parent"),
 };
 
@@ -9,8 +11,19 @@ DOMSelectors.form.addEventListener("submit", function (no) {
   no.preventDefault();
 });
 
-DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input}</p>`);
-  DOMSelectors.input.value = "";
+DOMSelectors.form.addEventListener("submit", function () {
+  let input1 = DOMSelectors.input1.value,
+    input2 = DOMSelectors.input2.value,
+    input3 = DOMSelectors.input3.value;
+  DOMSelectors.box.insertAdjacentHTML(
+    "afterend",
+    `<p>
+  ${input1}
+  ${input2}
+  ${input3}
+  </p>`
+  );
+  DOMSelectors.input1.value = "";
+  DOMSelectors.input2.value = "";
+  DOMSelectors.input3.value = "";
 });
