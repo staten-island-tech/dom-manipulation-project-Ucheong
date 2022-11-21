48e3a5b56af0243cbbe95e0ea30bc1a1323ac685
Ucheong <113154269+Ucheong@users.noreply.github.com>
2022-11-20 22:04:04 -0500

    DOM

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let DOMSelectors = {
   input2: document.querySelector(".box2"),
   input3: document.querySelector(".box3"),
   box: document.getElementById("form-parent"),
+  remove: document.querySelector(".remove"),
+  display: document.getElementById("display"),
 };
 
 DOMSelectors.form.addEventListener("submit", function (no) {
@@ -15,19 +17,20 @@ DOMSelectors.form.addEventListener("submit", function () {
   let input1 = DOMSelectors.input1.value,
     input2 = DOMSelectors.input2.value,
     input3 = DOMSelectors.input3.value;
-  DOMSelectors.box.insertAdjacentHTML(
-    "afterend",
+  DOMSelectors.display.insertAdjacentHTML(
+    "afterbegin",
     `<div class="display-card">
     <h3 class="display-sport">${input1}</h3>
     <img class="display-img" src="${input3}"/>
     <h2 class="display-team">${input2}</h2>
-    <button class="remove">Remove</button>
+    <button class="remove" id="btn">Remove</button>
     </div>`
   );
   DOMSelectors.input1.value = "";
   DOMSelectors.input2.value = "";
   DOMSelectors.input3.value = "";
-  .addEventListener("submit", function () {
-    delete DOMSelectors.input3;
+  DOMSelectors.display.addEventListener("click", function () {
+    const remove = document.querySelector(".display");
+    remove.remove();
   });
 });
